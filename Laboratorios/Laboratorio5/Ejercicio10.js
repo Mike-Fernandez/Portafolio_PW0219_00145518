3fd2a694cee6929cac868fa4fb36ab950c10232f
Mike-Fernandez <00145518@uca.edu.sv>
2019-09-27 10:48:18 -0600

    Add: Ejercicio 10

---
+++ Laboratorios/Laboratorio5/Ejercicio10.js
@@ -0,0 +1,10 @@
+//Ejercicio 10
+function abs(number){
+    if(number == 0){
+        return 0;
+    } else if(number < 0){
+        return number*(-1);
+    } else{
+        return number;
+    }
+}
